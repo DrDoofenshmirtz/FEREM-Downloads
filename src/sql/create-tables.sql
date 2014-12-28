30e7efdd9f7d9996ae362df2f86283c860315fa3
FrankM <frank.mosebach@gmx.de>
2014-12-28 19:57:01 +0100

    Removed name column from users table, limited length of mail address to 200.

--- src/sql/create-tables.sql
+++ src/sql/create-tables.sql
@@ -1,12 +1,10 @@
 create table users (
   id         uuid primary key,
-  name       varchar(80) not null,
-  e_mail     varchar(300) not null,
+  e_mail     varchar(200) not null,
   created_at timestamp not null,
-  unique(name, e_mail)
+  unique(e_mail)
 );
 
-create index users_name_idx on users(name);
 create index users_e_mail_idx on users(e_mail);
 
 create table downloads (
@@ -18,3 +16,4 @@ create table downloads (
 
 create index downloads_created_at_idx on downloads(created_at);
 create index downloads_downloaded_at_idx on downloads(downloaded_at);
+
