f8787b663c9868fc7e55077767e8010dd5802aa3
FrankM <frank.mosebach@gmx.de>
2015-01-04 00:18:12 +0100

    Replaced varchar with text type, initialize creation timestamps with default values.

--- src/sql/create-tables.sql
+++ src/sql/create-tables.sql
@@ -1,7 +1,7 @@
 create table users (
   id         uuid primary key,
-  e_mail     varchar(200) not null,
-  created_at timestamp not null,
+  e_mail     text not null,
+  created_at timestamp not null default current_timestamp,
   unique(e_mail)
 );
 
@@ -10,7 +10,7 @@ create index users_e_mail_idx on users(e_mail);
 create table downloads (
   id            uuid primary key,
   user_id       uuid references users(id) on delete cascade not null,
-  created_at    timestamp not null,
+  created_at    timestamp not null default current_timestamp,
   downloaded_at timestamp
 );
 
