11454346c49417bf30db07af89869bf04b1212d0
DrDoofenshmirtz <frank.mosebach@gmx.de>
2015-01-05 01:40:24 +0100

    Return the user id actually related to the given e-mail address.

--- src/sql/add-download.sql
+++ src/sql/add-download.sql
@@ -1,8 +1,9 @@
 create function add_download(_user_id text, _e_mail text, _download_id text)
-  -- The Racket driver somehow doesn't know how to handle 'void' results, hence 
-  -- we declare the return type as boolean and always return 'true'.
-  returns boolean as 
+  returns text as
 $$
+declare
+  _user_uuid uuid := cast(_user_id as uuid);
+  _download_uuid uuid := cast(_download_id as uuid);
 begin
 
   -- Lock the 'users' table to ensure an atomic 'insert if absent' without 
@@ -13,16 +14,17 @@ begin
   -- Insert a user row with the given user id and e-mail address unless a user
   -- with the same e-mail address already exists.
   insert into users (id, e_mail)
-    select cast(_user_id as uuid), _e_mail where not exists 
+    select _user_uuid, _e_mail where not exists 
       (select users.id from users where users.e_mail = _e_mail);
  
+  -- Acquire the user id actually related to the given e-mail address. 
+  _user_uuid := (select users.id from users where users.e_mail = _e_mail);    
+      
   -- Insert a download row for the user with the given e-mail, regardless if
   -- the user already existed or has just been newly created. 
-  insert into downloads (id, user_id)
-    select cast(_download_id as uuid), users.id from users 
-      where users.e_mail = _e_mail;
+  insert into downloads (id, user_id) values(_download_uuid, _user_uuid);
       
-  return true;
+  return cast(_user_uuid as text);
 end;
 $$ language plpgsql;
 
