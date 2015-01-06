85c1e2a1cd983ea8b4b79650b802f6d886ec5b05
FrankM <frank.mosebach@gmx.de>
2015-01-06 21:02:08 +0100

    Add custom file paths, connect to the db on start-up.

--- src/racket/app.rkt
+++ src/racket/app.rkt
@@ -3,13 +3,22 @@
 (provide run)
 
 (require web-server/servlet-env
+         "dbaccess.rkt"
          "actions.rkt")
 
-(struct app (name) #:transparent)
+(define (compile-paths working-directory)
+  (list (build-path working-directory "html")))
+
+(define (connect-to-db)
+  (connect-to "ferem" "ferem" "ferem"))
+
+(struct app (db-conn) #:transparent)
  
 (define (run working-directory)
-  (serve/servlet #:servlet-path    "/ferem-downloads"
-                 #:servlet-regexp  #rx"^/ferem-downloads.*$"
-                 #:port            17500
-                 #:launch-browser? #f
-                 (dispatcher (app "FEREM Downloads"))))
+  (serve/servlet #:servlet-path      "/ferem-downloads"
+                 #:servlet-regexp    #rx"^/ferem-downloads.*$"
+                 #:port              17500
+                 #:listen-ip         #f
+                 #:launch-browser?   #f
+                 #:extra-files-paths (compile-paths working-directory)
+                 (dispatcher (app (connect-to-db)))))
