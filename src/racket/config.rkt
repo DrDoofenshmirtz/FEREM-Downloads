ebb601e7a583e62d22c41690bb68b2ad152a1558
FrankM <frank.mosebach@gmx.de>
2015-04-08 22:15:26 +0200

    Stop processing the keys as soon as a key's value is absent.

--- src/racket/config.rkt
+++ src/racket/config.rkt
@@ -8,9 +8,13 @@
 (define (read-config-file path)
   (call-with-input-file path read-json #:mode 'text))
 
+(struct absent ())
+
 (define (config-value config keys [default #f])
   (if (empty? keys)
       config
-      (let ([key  (first keys)]
-            [keys (rest keys)])
-        (config-value (hash-ref config key default) keys default))))
+      (let* ([key   (first keys)]
+             [value (hash-ref config key (absent))])
+        (if (absent? value)
+            default
+            (config-value value (rest keys) default)))))
