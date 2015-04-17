a3b2f25fef85c093e7501140feb1d61d3b4c8817
FrankM <frank.mosebach@gmx.de>
2015-04-17 22:48:23 +0200

    Load the config on start, terminate on failure.

--- src/racket/app.rkt
+++ src/racket/app.rkt
@@ -11,8 +11,16 @@
 (define (config-path working-directory)
   (build-path working-directory "config" "config.json"))
 
+(define (load-config-failed error)
+  (log-frmdls-error "Failed to load config file (error: ~a)!" 
+                    (exn-message error))
+  #f)
+
 (define (load-config working-directory)
-  (read-config-file (config-path working-directory)))
+  (let ([config-path (config-path working-directory)])
+    (with-handlers ([exn:fail? load-config-failed])
+      (log-frmdls-info "Loading config (file: ~a)." config-path)
+      (read-config-file config-path))))
 
 (define (compile-paths working-directory)
   (list (build-path working-directory)))
@@ -27,15 +35,21 @@
   (close-writers)
   exit-code)
 
+(define (exit-with-failure)
+  (exit (close-log-writers-on-exit 1)))
+
 (define (run working-directory)
   (executable-yield-handler close-log-writers-on-exit)
   (attach-writer (console-writer) 'info)
   (log-frmdls-info "Started FEREM Downloads server (working directory: ~a)." 
                    working-directory)
-  (serve/servlet #:servlet-path      "/ferem-downloads"
-                 #:servlet-regexp    #rx"^/ferem-downloads.*$"
-                 #:port              17500
-                 #:listen-ip         #f
-                 #:launch-browser?   #f
-                 #:extra-files-paths (compile-paths working-directory)
-                 (dispatcher (app (connect-to-db)))))
+  (let ([config (load-config working-directory)])
+    (if config
+        (serve/servlet #:servlet-path      "/ferem-downloads"
+                       #:servlet-regexp    #rx"^/ferem-downloads.*$"
+                       #:port              17500
+                       #:listen-ip         #f
+                       #:launch-browser?   #f
+                       #:extra-files-paths (compile-paths working-directory)
+                       (dispatcher (app (connect-to-db))))
+        (exit-with-failure))))
