953dda1940276ccdacc72a1835eb85364c1e2a0c
DrDoofenshmirtz <frank.mosebach@gmx.de>
2015-04-22 09:14:37 +0200

    Added comments for documentation.

--- src/sql/create-database.sql
+++ src/sql/create-database.sql
@@ -1,8 +1,17 @@
+-- Assuming you've opened a shell in this file's directory, you can create the
+-- database for the FEREM Downloads application by executing
+--
+--   psql -U postgres -f create-database.sql
+--
+-- on the command line.
+
+-- Create the role to be used as owner of our database.
 create role deinc 
   login
   password 'dunkeldorf'
   superuser inherit createdb nocreaterole noreplication;
 
+-- Create the database to be used for FEREM Downloads.  
 create database ferem
   with owner            = deinc
        encoding         = 'UTF8'
