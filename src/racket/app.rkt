184459d08d50ed861baf657a6e6a8284d7f81d7d
FrankM <frank.mosebach@gmx.de>
2015-04-24 23:24:16 +0200

    Configure the app (db connection options, server port) using a JSON config file.
    The config file is loaded from a predefined path at application startup
    (<working-directory>/config/config.json).

--- src/racket/app.rkt
+++ src/racket/app.rkt
@@ -11,24 +11,42 @@
 (define (config-path working-directory)
   (build-path working-directory "config" "config.json"))
 
-(define (load-config-failed error)
-  (log-frmdls-error "Failed to load config file (error: ~a)!" 
+(define (load-config working-directory)
+  (let ([config-path (config-path working-directory)])
+    (log-frmdls-info "...loading config (file: ~a)..." config-path)
+    (read-config-file config-path)))
+
+(define (connect-to-db config)
+  (log-frmdls-info "...connecting to the database...")
+  (let* ([db-config (get-db-config config)]
+         [server    (db-config-server db-config)]
+         [port      (db-config-port db-config)]
+         [database  (db-config-database db-config)]
+         [user      (db-config-user db-config)]
+         [password  (db-config-password db-config)])
+    (connect-to database user password #:server server #:port port)))
+
+(struct app (port db-conn) #:transparent)
+
+(define (set-up config db-conn)
+  (log-frmdls-info "...setting up the application...")
+  (let* ([app-config (get-app-config config)]
+         [port       (app-config-port app-config)])
+    (app port db-conn)))
+
+(define (boot-failed error)
+  (log-frmdls-error "Failed to boot the application (error: ~a)!"
                     (exn-message error))
   #f)
 
-(define (load-config working-directory)
-  (let ([config-path (config-path working-directory)])
-    (with-handlers ([exn:fail? load-config-failed])
-      (log-frmdls-info "Loading config (file: ~a)." config-path)
-      (read-config-file config-path))))
-
-(define (compile-paths working-directory)
-  (list (build-path working-directory)))
-
-(define (connect-to-db)
-  (connect-to "ferem" "ferem" "ferem"))
-
-(struct app (db-conn) #:transparent)
+(define (boot working-directory)
+  (log-frmdls-info "Booting the application...")
+  (with-handlers ([exn:fail? boot-failed])
+    (let* ([config  (load-config working-directory)]
+           [db-conn (connect-to-db config)]
+           [app     (set-up config db-conn)])
+      (log-frmdls-info "~a" app)  
+      app)))
 
 (define (close-log-writers-on-exit exit-code)
   (log-frmdls-info "Closing log writers on exit...")
@@ -38,18 +56,21 @@
 (define (exit-with-failure)
   (exit (close-log-writers-on-exit 1)))
 
+(define (compile-paths working-directory)
+  (list (build-path working-directory)))
+
 (define (run working-directory)
   (executable-yield-handler close-log-writers-on-exit)
   (attach-writer (console-writer) 'info)
   (log-frmdls-info "Started FEREM Downloads server (working directory: ~a)." 
                    working-directory)
-  (let ([config (load-config working-directory)])
-    (if config
+  (let ([app (boot working-directory)])
+    (if app
         (serve/servlet #:servlet-path      "/ferem-downloads"
                        #:servlet-regexp    #rx"^/ferem-downloads.*$"
-                       #:port              17500
+                       #:port              (app-port app)
                        #:listen-ip         #f
                        #:launch-browser?   #f
                        #:extra-files-paths (compile-paths working-directory)
-                       (dispatcher (app (connect-to-db))))
+                       (dispatcher app))
         (exit-with-failure))))
